2747798a46bcbb212df0999600d2778a920240b3
martin <martdevelopers254@gmail.com>
2020-06-19 17:24:36 +0300

    Implemented BillBoard Advertising Inc Template

---
+++ BAF/js/main.js
@@ -0,0 +1,37 @@
+function index()
+{
+    document.getElementById("title").innerHTML = "BillBoard Advertising Inc · Home Page";
+    document.getElementById("app_name").innerHTML = "BillBoard Advertising Inc.";
+    document.getElementById("app_name_1").innerHTML = "BillBoard Advertising Inc.";
+    document.getElementById("app_desc").innerHTML = "BillBoard Advertising Inc. is widely recognised as the pan-African leader in outdoor and airport advertising, having the widest coverage on the African continent.Operating in over 23 countries, with 20 years experience, we have been awarded the European Union Award for innovative media and the prestigious Best Outdoor Advertising Company award in multiple markets for the past ten consecutive years." ;
+    document.getElementById("app_products").innerHTML ="View Our Products";
+    document.getElementById("app_contact").innerHTML = "Contact Us" 
+    document.getElementById("app_top").innerHTML = "Back to top";
+    document.getElementById("app_index-1").src = "img/billboard.jpg";
+    document.getElementById("app_index-1-info").innerHTML = "Billboard Advertising ";
+    document.getElementById("app_index-2").src = "img/spectular.jpg";
+    document.getElementById("app_index-2-info").innerHTML = "Spectacular Billboards";
+    document.getElementById("app_index-3").src = "img/airport.jpg";
+    document.getElementById("app_index-3-info").innerHTML = "Airport Advertising  ";
+    document.getElementById("app_index-4").src = "img/outdoor.jpg";
+    document.getElementById("app_index-4-info").innerHTML = "Landmark Outdoor";
+    document.getElementById("app_index-5").src = "img/functure.jpg";
+    document.getElementById("app_index-5-info").innerHTML = "Street Furniture ";
+    document.getElementById("app_index-6").src = "img/mall.jpg";
+    document.getElementById("app_index-6-info").innerHTML = "Mall Media";
+
+
+
+
+
+}
+
+function navigation()
+{
+    document.getElementById("products").innerHTML = "Products";
+    document.getElementById("about").innerHTML = "About";
+    document.getElementById("gallery").innerHTML = "Gallery";
+    document.getElementById("artwork").innerHTML = "ArtWork";
+    document.getElementById("contact").innerHTML = "Contact Us";
+    document.getElementById("app_footer").innerHTML = "BillBoard Advertisment Inc. All rights reserved.";
+}
