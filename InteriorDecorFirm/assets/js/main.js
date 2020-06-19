38f7220b628648a26e7a5ca24860bff07321678a
martin <martdevelopers254@gmail.com>
2020-06-19 14:10:22 +0300

    Fully implemented all pages

---
+++ InteriorDecorFirm/assets/js/main.js
@@ -0,0 +1,149 @@
+//indicate messange has been sent
+
+
+//index.html inject doms
+function indexPage()
+{
+  document.getElementById("heading").innerHTML = "TilesSales N` Installation ";
+  document.getElementById("tagline-1").innerHTML = "A professional team that gets the work done";
+  document.getElementById("cover-heading").innerHTML = "TilesSales N` Installation Inc. is a group of skilled tile sellers and installers who bring your décor visions to life. Whether you need a tiling solution for your kitchens, baths, living areas, fireplaces, or patios, we have you covered!";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation. All Rights reserved";
+
+}
+
+//about us inject doms
+
+function aboutusPage()
+{
+  document.getElementById("about1").innerHTML = "Tiling Craftsman At Your Service";
+  document.getElementById("about1.1").innerHTML = "We install tiles that inspire for various residential and commercial spaces. Through a combination of artistic design and clean and precise installation techniques, TilesSales N' Installation Inc. provides you with excellence in every tile project, be it big or small.";
+  document.getElementById("about2").innerHTML = "Laying Tile Surfaces With Perfection Since 2020";
+  document.getElementById("about2.2").innerHTML = "As tiling contractors we are dedicated to bringing superior craftsmanship to each of our tile projects. Our team of experienced and qualified tilers are trained to do the job right the first time. Whatever tiling trends inspire you, our experts can incorporate these trends into your tile project.";
+  document.getElementById("about3").innerHTML = "Defined By Innovation, Driven By Customer Satisfaction";
+  document.getElementById("about3.3").innerHTML = "Whether it's a renovation, a new construction or a commercial project, TilesSales N` Installation Inc. will take care of all your tiling projects. We specialize in the installation of various types of surfaces: ceramic, porcelain, glass, marble, granite, slate or natural stone. We are committed to providing tile installation services that meet all your tile needs!";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+
+}
+
+//Services  inject dom
+
+function services()
+{
+  //Services 1
+  document.getElementById("serv-1-img").src = "assets/img/service-1.jpg";
+  document.getElementById("serv-1").innerHTML = "Flooring";
+  document.getElementById("serv-1-info").innerHTML = "If you want the floor of your space to stand out, our modern installation methods are a source of inspiration for those looking for a clean, elegant and sustainable floor design.";
+  document.getElementById("requestQuote").innerHTML= "Request An Estimate";
+
+  document.getElementById("serv-2-img").src = "assets/img/service-2.jpg";
+  document.getElementById("serv-2").innerHTML = "Wall Tiling";
+  document.getElementById("serv-2-info").innerHTML = "Tiling your walls helps you get the look you dream of! Beautifully finished walls can add a special ambiance to any room in your home, we can do all your work.";
+  
+
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+
+
+}
+
+function services_2()
+{
+  //services page 2
+  document.getElementById("serv-3-img").src = "assets/img/service-4.jpg";
+  document.getElementById("serv-3").innerHTML = "Countertop Installation";
+  document.getElementById("serv-3-info").innerHTML = "From kitchen backsplash to counter-tops, our tile experts can refresh your kitchen’s look in the most interesting ways. We install durable, easy to maintain, elegant kitchen counters.";
+
+  document.getElementById("serv-4-img").src = "assets/img/service-3.jpg";
+  document.getElementById("serv-4").innerHTML = "Fireplace Tiling";
+  document.getElementById("serv-4-info").innerHTML = "From traditional masonry to modern marble tiles, you have unlimited options to customize your fireplace in a way that reflects your lifestyle. Be it an outdoor fireplace or indoor we will make sure to satisfy your taste.";
+
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+}
+
+
+function services_3()
+{
+  //services_3 page 
+  document.getElementById("serv-5-img").src = "assets/img/service-5.jpg";
+  document.getElementById("serv-5").innerHTML = "Complete Home Tiling";
+  document.getElementById("serv-5-info").innerHTML = "If you are building a new home or renovating your current home, with us, you are in good hands for all your tiling needs.We offer  affordable tile sale and installation services.";
+
+  document.getElementById("serv-6-img").src = "assets/img/service-6.jpg";
+  document.getElementById("serv-6").innerHTML = "Commercial Tiling";
+  document.getElementById("serv-6-info").innerHTML = "From small offices to large offices, our tiling experts can help you make an impact on your customers. Our tiling experts can help you bring all of your design ideas to life.";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+}
+
+function gallery()
+{
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+  document.getElementById("gallery-1").src="assets/img/service-1.jpg"
+  document.getElementById("gallery-2").src="assets/img/service-2.jpg"
+  document.getElementById("gallery-3").src="assets/img/service-3.jpg"
+  document.getElementById("gallery-4").src="assets/img/service-4.jpg"
+
+}
+
+function gallery_2()
+{
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+  document.getElementById("gallery-5").src ="assets/img/service-5.jpg";
+  document.getElementById("gallery-6").src ="assets/img/service-6.jpg";
+  document.getElementById("gallery-7").src ="assets/img/service-7.jpg";
+  document.getElementById("gallery-8").src ="assets/img/service-8.jpg";
+}
+
+function contact()
+{
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+  document.getElementById("business-info").innerHTML = "Business Information";
+  document.getElementById("bsn-name").innerHTML = "TilesSales N' Installation Inc.";
+
+}
+
+function tilesShop()
+{
+  document.getElementById("p1").innerHTML = "Our Tiles Categories and types";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+
+  document.getElementById("shop-1-img").src = "assets/img/shop-1.webp";
+  document.getElementById("shop-1").innerHTML = "Slim Ayton Grey Glazed Porcelain Floor Tile <br> 600 x 600mm";
+  document.getElementById("shop-1-info").innerHTML = "Porcelain Floor Tile KES 1,720 / m2 ";
+
+  document.getElementById("shop-2-img").src = "assets/img/shop-2.webp";
+  document.getElementById("shop-2").innerHTML = "Fast Avorio Glazed Porcelain Floor Tile  300 x 600mm ";
+  document.getElementById("shop-2-info").innerHTML = "Glazed Porcelain Tile  KES 1,599 / m2";
+}
+
+function tilesShop_2()
+{
+  document.getElementById("p1").innerHTML = "Our Tiles Categories and types";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+
+  document.getElementById("shop-3-img").src = "assets/img/shop-4.webp";
+  document.getElementById("shop-3").innerHTML = "Emerald Green Ceramic Floor Tile <br> 400 x 400mm ";
+  document.getElementById("shop-3-info").innerHTML = "Ceramic Tile KES 1,031 / m2";
+
+  document.getElementById("shop-4-img").src = "assets/img/shop-5.webp";
+  document.getElementById("shop-4").innerHTML = "Marbella Pearl Ivory Shiny Glazed Porcelain Floor Tile <br> 450 x 900mm";
+  document.getElementById("shop-4-info").innerHTML = "Shiny Glazed Finish KES 2,358 / m2 ";
+}
+
+function tilesShop_3()
+{
+  document.getElementById("p1").innerHTML = "Our Tiles Categories and types";
+  document.getElementById("footerInfo").innerHTML = "TilesSales N` Installation.. All rights reserved ";
+
+  document.getElementById("shop-5-img").src = "assets/img/shop-3.webp";
+  document.getElementById("shop-5").innerHTML = "Emerald Blue Ceramic Floor Tile <br> 400 x 400mm  ";
+  document.getElementById("shop-5-info").innerHTML = "Ceramic Tile  <br> KES 1,031 / m2";
+
+  document.getElementById("shop-6-img").src = "assets/img/shop-6.webp";
+  document.getElementById("shop-6").innerHTML = "Downtown Mid-Grey Glazed Porcelain Floor Tile <br> 800 x 800mm";
+  document.getElementById("shop-6-info").innerHTML = "Glazed Porcelain Tile KES 1,031 / m2";
+}
+
+
+function sendMessange()
+  {
+    window.alert("Messange Send");
+  }
