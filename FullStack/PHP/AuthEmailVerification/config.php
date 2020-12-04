4c141f3bc6235f6326127547b8696847f37e3fb8
MartMbithi <martdevelopers254@gmail.com>
2020-12-04 08:21:16 +0300

    Email Verification Sanitized

--- FullStack/PHP/AuthEmailVerification/config.php
+++ FullStack/PHP/AuthEmailVerification/config.php
@@ -2,11 +2,10 @@
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME', 'demos');
+define('DB_NAME', 'email_verifier');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>
